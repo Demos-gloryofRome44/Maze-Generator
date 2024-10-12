5996eacf7c501eeec573769f6bfda5f6be32e674
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-12 18:31:45 +0300

    Added enum for solver maze

---
+++ src/main/java/backend/academy/AlgoritmsSolver.java
@@ -0,0 +1,24 @@
+package backend.academy;
+
+public enum AlgoritmsSolver {
+    BFS(1) ,
+    DFS(2);
+
+    private final int value;
+    AlgoritmsSolver(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static AlgoritmsSolver fromValue(int value) {
+        for (AlgoritmsSolver topic : AlgoritmsSolver.values()) {
+            if (topic.getValue() == value) {
+                return topic;
+            }
+        }
+        throw new IllegalArgumentException("Нет алгоритма с таким значением: " + value);
+    }
+}
