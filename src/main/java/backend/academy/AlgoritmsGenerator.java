3dda84e40d81d80774441fbbdecfec067f70d2e1
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-12 18:26:45 +0300

    Added enum for generator maze

---
+++ src/main/java/backend/academy/AlgoritmsGenerator.java
@@ -0,0 +1,22 @@
+package backend.academy;
+
+public enum AlgoritmsGenerator {
+    IDEAL(1) ,
+    KRUSKAL(2);
+
+    private final int value;
+    AlgoritmsGenerator(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    public static AlgoritmsGenerator fromValue(int value) {
+        for (AlgoritmsGenerator topic : AlgoritmsGenerator.values()) {
+            if (topic.getValue() == value) {
+                return topic;
+            }
+        }
+        throw new IllegalArgumentException("Нет алгоритма с таким значением: " + value);
+    }
+}
