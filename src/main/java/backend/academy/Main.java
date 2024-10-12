086abf4e0a732fdfd62c9a029ae267313aa19052
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-12 22:52:16 +0300

    Added a user interface for generating a maze and selecting algorithms.

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,76 @@
 package backend.academy;
 
 import lombok.experimental.UtilityClass;
+import java.util.List;
+import java.util.Scanner;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Учтите, прежде чем генерировать лабиринт, что его размер подходит под вашу консоль\n" +
+            "рекомендованное максимально число на высоту - 35 и ширину - 170");
+        int height = InputValidator.validateHeight(scanner, System.out);
+        int width = InputValidator.validateWidth(scanner, System.out);
+
+        int choiceGenerator = InputValidator.validateAlgoritmGeneratorChoice(scanner, System.out);
+        AlgoritmsGenerator algoritm = AlgoritmsGenerator.fromValue(choiceGenerator);
+
+        Maze maze;
+        switch (algoritm) {
+            case KRUSKAL:
+                KruskalGenerator generator = new KruskalGenerator();
+                maze = generator.generate(height, width);
+                System.out.println("Лабиринт сгенерирован с использованием алгоритма Крускала.");
+                break;
+            case IDEAL:
+                IdealGenerator idealgenerator = new IdealGenerator();
+                maze = idealgenerator.generate(height, width);
+                System.out.println("Лабиринт сгенерирован с использованием идеального алгоритма.");
+                break;
+            default:
+                throw new IllegalStateException("Неизвестный алгоритм: " + algoritm);
+        }
+
+
+
+        System.out.println("Сгенерированный лабиринт:");
+        ConsoleRenderer renderer = new ConsoleRenderer();
+        System.out.println(renderer.render(maze));
+
+        Coordinate startPoint = InputValidator.validateCoordinate(scanner, height, width,
+            "начальную", "A", System.out);
+
+
+        Coordinate endPoint = InputValidator.validateCoordinate(scanner, height, width,
+            "конечную", "B", System.out);
+
+
+        int choiceSolver = InputValidator.validateSolverChoice(scanner, System.out);
+
+        AlgoritmsSolver algoritmsSolver = AlgoritmsSolver.fromValue(choiceSolver);
+        List<Coordinate> path;
+        switch (algoritmsSolver) {
+            case BFS:
+                BFSSolver bfsSolver = new BFSSolver();
+                path = bfsSolver.solve(maze, startPoint, endPoint);
+                break;
+            case DFS:
+                DFSSolver dfsSolver = new DFSSolver();
+                path = dfsSolver.solve(maze, startPoint, endPoint);
+                break;
+            default:
+                throw new IllegalStateException("Неизвестный алгоритм: " + algoritm);
+        }
+
+        if (!path.isEmpty()) {
+            System.out.println("Найденный путь:");
+            System.out.println(renderer.renderWithPath(maze, path, startPoint, endPoint));
+        } else {
+            System.out.println("Путь не найден.");
+        }
+
+        scanner.close();
     }
 }
