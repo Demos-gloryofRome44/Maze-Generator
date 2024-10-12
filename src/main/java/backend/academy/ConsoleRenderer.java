0d2aa71df2c623a1a19d0e05c243c6c2c7b9f38b
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-12 17:54:06 +0300

    Added renderWithPath method to visualize the maze with a specified path

--- src/main/java/backend/academy/ConsoleRenderer.java
+++ src/main/java/backend/academy/ConsoleRenderer.java
@@ -1,5 +1,7 @@
 package backend.academy;
 
+import java.util.List;
+
 public class ConsoleRenderer {
     public String render(Maze maze) {
         StringBuilder sb = new StringBuilder();
@@ -9,6 +11,32 @@ public class ConsoleRenderer {
             }
             sb.append("\n");
         }
+        return sb.toString();
+    }
+
+    public String renderWithPath(Maze maze, List<Coordinate> path, Coordinate start, Coordinate end) {
+        StringBuilder sb = new StringBuilder();
+        char[][] visualMaze = new char[maze.getHeight()][maze.getWidth()];
+
+        for (int row = 0; row < maze.getHeight(); row++) {
+            for (int col = 0; col < maze.getWidth(); col++) {
+                visualMaze[row][col] = maze.getGrid()[row][col].getType() == Cell.Type.WALL ? '█' : ' ';
+            }
+        }
+
+        visualMaze[start.row()][start.col()] = 'A';
+        visualMaze[end.row()][end.col()] = 'B';
+
+        for (Coordinate coordinate : path) {
+            if (!(coordinate.equals(start) || coordinate.equals(end))) {
+                visualMaze[coordinate.row()][coordinate.col()] = '*';
+            }
+        }
+
+        for (int row = maze.getHeight() - 1; row >= 0; row--) { 
+            sb.append(new String(visualMaze[row])).append("\n");
+        }
+
         return sb.toString();
     }
 }
