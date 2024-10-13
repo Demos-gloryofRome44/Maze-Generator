0dd7b2c98218b3e09c70b3460010f60c4034bd7d
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-13 20:07:13 +0300

    Update combining the processing of pathfinding algorithms into one test

--- src/test/java/backend/academy/samples/SolverTest.java
+++ src/test/java/backend/academy/samples/SolverTest.java
@@ -3,6 +3,7 @@ package backend.academy.samples;
 import backend.academy.BFSSolver;
 import backend.academy.Cell;
 import backend.academy.Coordinate;
+import backend.academy.DFSSolver;
 import backend.academy.Maze;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -10,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BFSSolverTest {
+public class SolverTest {
 
     // Тест что алгоритм нашел путь между двумя точками правильно
     @Test
@@ -19,11 +20,19 @@ public class BFSSolverTest {
         Coordinate start = new Coordinate(0, 0);
         Coordinate end = new Coordinate(4, 4);
 
+        // проверка алгоритма BFS
         BFSSolver bfsSolver = new BFSSolver();
-        List<Coordinate> path = bfsSolver.solve(maze, start, end);
+        List<Coordinate> pathbfs = bfsSolver.solve(maze, start, end);
 
-        assertFalse(path.isEmpty(), "Path should exist but was not found.");
-        assertEquals(end, path.get(path.size() - 1), "The last coordinate of the path should be the end coordinate.");
+        assertFalse(pathbfs.isEmpty(), "Path should exist but was not found.");
+        assertEquals(end, pathbfs.get(pathbfs.size() - 1), "The last coordinate of the path should be the end coordinate.");
+
+        //проверка алгоритма DFS
+        DFSSolver dfsSolver = new DFSSolver();
+        List<Coordinate> pathdfs = dfsSolver.solve(maze, start, end);
+
+        assertFalse(pathdfs.isEmpty(), "Path should exist but was not found.");
+        assertEquals(end, pathdfs.get(pathbfs.size() - 1), "The last coordinate of the path should be the end coordinate.");
     }
 
     // Тест что алгоритм не нашел путь между двумя точками
@@ -33,10 +42,17 @@ public class BFSSolverTest {
         Coordinate start = new Coordinate(0, 0);
         Coordinate end = new Coordinate(4, 4);
 
+        // проверка алгоритма BFS
         BFSSolver bfsSolver = new BFSSolver();
-        List<Coordinate> path = bfsSolver.solve(maze, start, end);
+        List<Coordinate> pathbfs = bfsSolver.solve(maze, start, end);
 
-        assertTrue(path.isEmpty(), "Path should not exist but was found.");
+        assertTrue(pathbfs.isEmpty(), "Path should not exist but was found.");
+
+        // проверка алгоритма DFS
+        DFSSolver dfsSolver = new DFSSolver();
+        List<Coordinate> pathdfs = dfsSolver.solve(maze, start, end);
+
+        assertTrue(pathdfs.isEmpty(), "Path should not exist but was found.");
     }
 
     private Maze createMazeWithPath() {
