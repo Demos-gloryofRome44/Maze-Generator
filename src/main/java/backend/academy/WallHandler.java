31f8b9ea682a70347fcb9791af5afa9ee5515edf
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2024-10-13 10:23:41 +0300

    Fix output stream

--- src/main/java/backend/academy/WallHandler.java
+++ src/main/java/backend/academy/WallHandler.java
@@ -1,5 +1,6 @@
 package backend.academy;
 
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class WallHandler {
@@ -11,7 +12,9 @@ public class WallHandler {
         this.scanner = new Scanner(System.in);
     }
 
-    public Coordinate checkAndModifyCellType(Coordinate coord, String pointLabel) {
+    @SuppressWarnings("all")
+    public Coordinate checkAndModifyCellType(Coordinate coordinate, String pointLabel) {
+        Coordinate coord = coordinate;
         while (maze.getCell(coord.row(), coord.col()).getType() != Cell.Type.PASSAGE) {
             System.out.printf("Вы выбрали за точку %s стену. Вы хотите сделать её проходом? Да/Нет? ", pointLabel);
             String response = scanner.nextLine();
@@ -20,14 +23,15 @@ public class WallHandler {
                 maze.getCell(coord.row(), coord.col()).setType(Cell.Type.PASSAGE);
                 break;
             } else {
-                coord = getUserInputCoordinate(String.format("Введите новые координаты для точки %s (row col): ", pointLabel));
+                coord = getUserInputCoordinate(String.format("Введите новые координаты для точки %s (row col): ",
+                    pointLabel), System.out);
             }
         }
         return coord;
     }
 
-    private Coordinate getUserInputCoordinate(String prompt) {
-        System.out.print(prompt);
+    private Coordinate getUserInputCoordinate(String prompt, PrintStream out) {
+        out.print(prompt);
         int row = scanner.nextInt();
         int col = scanner.nextInt();
         return new Coordinate(row, col);
